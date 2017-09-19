6b663afd7ac8332002fc3fa293a57c859aadb173
Wang <swang41@student.cccd.edu>
2017-09-19 15:10:33 -0700

    Initial commit

---
+++ app/src/main/java/edu/orange/cs273/swang41/paintestimator/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.orange.cs273.swang41.paintestimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
